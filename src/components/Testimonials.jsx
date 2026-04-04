a12879e6ba5097beaa0bdbd5cc8458cf561d1904
rahullohchab5210 <rahullohchab5210@gmail.com>
2026-04-04 10:31:31 +0530

    updated section

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -8,7 +8,7 @@ import { Navigation } from "swiper/modules";
 
 function Testimonials() {
     return (
-        <div className="bg-[url('/assets/images/png/testimonials-bg.png')] bg-cover bg-center  pt-[167px] bg-[#220933]/90 w-full h-full">
+        <div className="bg-[url('/assets/images/png/testimonials-bg.png')] bg-cover bg-center pb-25 pt-[167px] bg-[#220933]/90 w-full h-full">
 
             {/* HEADING + ARROWS */}
             <div className="flex items-center max-w-287 mx-auto justify-between mb-7.75 max-[1200px]:px-3  max-sm:px-5.5 ">
@@ -27,21 +27,23 @@ function Testimonials() {
                 </div>
             </div>
 
-            <div className="max-w-[1460px]   min-[1200px]:pl-[140px] max-[1200px]:pl-3 overflow-hidden  mx-auto">
+            <div className="sm:max-w-[1460px] max-w-[1140px]   min-[1200px]:pl-[140px] max-[1200px]:pl-5 overflow-hidden  mx-auto">
                 <div className="flex max-sm:flex-col sm:gap-[21px] max-sm:gap-y-8.25">
 
                     {/* LEFT STATIC FIXED CARD (NO SLIDE) */}
-                    <div className="bg-[url('/assets/images/png/testimonial-card-bg.png')] bg-cover bg-center sm:max-w-[271px] max-w-full max-sm:mr-10 max-sm:ml-7 max-sm:rounded-3xl max-sm:pr-4 h-[366px] pt-[50.5px] shrink-0">
+                    <div className=" max-sm:pr-15"> 
+                    <div className="bg-[url('/assets/images/png/testimonial-card-bg.png')] bg-cover bg-center sm:w-[271px] w-full  max-sm:ml-5 max-sm:rounded-3xl    h-[366px] pt-[50.5px] sm:shrink-0">
                         <div className='flex flex-col items-center'>
                             <div className='flex flex-col items-center gap-2.75'>
                                 <h6 className='font-medium text-[32px] text-white font-poppins'>Excellent</h6>
                                 <img src="/assets/images/png/icon-w.png" className='w-[107px] h-[104px]' />
                                 <Icons icon={"BIG_STAR"} />
                             </div>
-                            <p className='pt-2 pb-2 font-medium text-base text-[#F5F5F5]'>
+                            <p className='pt-2 pb-2 font-medium text-base text-[#F5F5F5] max-w-full sm:max-w-[271px]'>
                                 Based on <span className='underline'>456 reviews</span>
                             </p>
                             <Icons icon={"TRUSTPILOT"} />
+                        </div>
                         </div>
                     </div>
 
@@ -60,21 +62,21 @@ function Testimonials() {
                     >
                         {TESTIMONIALS_DATA.map((item, i) => (
                             <SwiperSlide key={i} style={{ width: "368px" }}>
-                                <div className='px-5 py-[31.5px] border-[1.5px] border-white/10 rounded-3xl backdrop-blur-[50px] sm:max-w-[368px] max-w-[319px] min-h-[366px]'>
+                                <div className='px-5 py-[31.5px] border-[1.5px] border-white/10 rounded-3xl backdrop-blur-[50px] sm:max-w-[368px] max-w-[319px] max-h-[366px]'>
                                     <div className='flex items-center justify-between'>
                                         <Icons icon={"SMALL_STAR"} />
-                                        <span className='font-medium text-xl text-[#F5F5F5]'>{item.date}</span>
+                                        <span className='font-medium sm:text-xl text-sm text-[#F5F5F5]'>{item.date}</span>
                                     </div>
 
-                                    <h6 className='font-medium text-[32px] text-white font-poppins pb-2 pt-[81px]'>
+                                    <h6 className='font-medium sm:text-[32px] text-[28px] text-white font-poppins pb-2 pt-[81px]'>
                                         {item.title}
                                     </h6>
 
-                                    <p className='font-medium text-base text-[#F5F5F5] pb-[81px]'>
+                                    <p className='font-medium sm:text-base text-sm text-[#F5F5F5] pb-[81px]'>
                                         {item.description}
                                     </p>
 
-                                    <h6 className='font-bold text-xl text-[#F5F5F5]'>{item.name}</h6>
+                                    <h6 className='font-bold sm:text-xl text-[18px] text-[#F5F5F5]'>{item.name}</h6>
                                 </div>
                             </SwiperSlide>
                         ))}
